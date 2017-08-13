93ae9e8046cb771902b4721b9c41c3794fd37ed8
ruoru <cruoru@gmail.com>
2017-08-14 01:34:46 +0800

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'js/[name].bundle.[hash:6].js',
+    },
+    module: {
+        loaders: [
+            //处理js文件
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader',
+                exclude: [/node_modules/],  //不通过该babel处理，提高打包速度。
+                include: [/src/],           //指定打包范围
+                query: {
+                    presets: ['latest'],
+                },
+            },
+            //处理css文件
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1 // 0 => no loaders (default); 1 => postcss-loader; 2 => postcss-loader, sass-loader. 在css-loader之后，指定几个loader处理import的css
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                //require('autoprefixer')
+                            ]
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.(jpg|png|jpeg|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        query: {
+                            limit: 100000,    //设定最小值，小于最小值可图片被打包成dataURL base64编码
+                            name: 'image/[name].[ext]'
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: 'public/index.html',
+            filename: 'index.html',
+            inject: 'body',
+        }),
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,    //启用所有服务的gzip压缩
+        host: '0.0.0.0',
+        port: 8001,
+        
+        //lazy: true,    //当lazy启用时，当它被请求的DEV-服务器将只编译软件包。这意味着webpack不会看到任何文件更改。我们称这个懒惰模式。
+        //filename: "[name].bundle.js",    ///[name].bundle.js请求时才编译 。filename在没有延迟模式的情况下使用时不起作用。
+    },
+};
